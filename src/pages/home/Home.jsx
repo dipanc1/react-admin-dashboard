f2a22f13dea6cc151bbd66eb50375b18b7ef7fc8
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-10 15:20:44 +0530

    added folder content and installed sass

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './home.scss'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className="home">
+            <div className="title">
+                Home
+            </div>
+        </div>
+    )
 }
 
 export default Home
