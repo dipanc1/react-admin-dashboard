6eaee6a00f34a4410ce1a02030f9f9eeef456a98
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-10 15:54:08 +0530

    installed react router dom, set it in app.js and made new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users">
+            <Route index element={<List />} />
+            <Route path=":id" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="/products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
