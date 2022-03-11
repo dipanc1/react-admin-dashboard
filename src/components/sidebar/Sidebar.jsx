081c698f5fb704b96c2c29bd5174fbfb1b1af740
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-11 14:50:47 +0530

    side bar css, adding material icons and stuff

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { DashboardSharp, ShoppingBag, ShoppingCart, NotificationsActive, Healing, List, Settings, PersonOutline, Logout, PersonPin, DeliveryDining, Details } from '@mui/icons-material'
 import React from 'react'
 import './sidebar.scss'
 
@@ -6,23 +7,64 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className="top">
         <span className="logo">
-          Dipan Admin.
+          Chicken Farm.
         </span>
       </div>
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardSharp className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PersonPin className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <ShoppingCart className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <ShoppingBag className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <DeliveryDining className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL LINKS</p>
+          <li>
+            <NotificationsActive className="icon" />
+            <span>Notifications</span>
+          </li>
+          <li>
+            <Details className="icon" />
+            <span>Stats</span>
+          </li>
+          <div className="title">SERVICE</div>
+          <li>
+            <Healing className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <List className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <Settings className="icon" />
+            <span>Settings</span>
+          </li>
+          <div className="title">USER</div>
+          <li>
+            <PersonOutline className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <Logout className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
