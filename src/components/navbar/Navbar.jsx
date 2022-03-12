ae2573a3cfa521699d6b725e9897d8c84de8b402
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-12 15:33:37 +0530

    few changes in navabr css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,35 @@
+import { ChatBubbleOutlineOutlined, DarkModeOutlined, FullscreenExit, LanguageOutlined, ListOutlined, SearchOutlined } from '@mui/icons-material'
 import React from 'react'
 import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlined />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined/>
+            English 
+          </div>
+          <div className="item">
+            <DarkModeOutlined/>
+          </div>
+          <div className="item">
+            <FullscreenExit/> 
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined/> 
+          </div>
+          <div className="item">
+            <ListOutlined/> 
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
