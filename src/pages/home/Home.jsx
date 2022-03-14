99aa0b0ea69e48e0cdbe51514ec5a9323748a8ee
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-14 16:52:23 +0530

    mui table ready

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from '../../components/navbar/Navbar'
 import Widget from '../../components/widget/Widget'
 import Featured from '../../components/featured/Featured'
 import Chart from '../../components/chart/Chart'
+import List from '../../components/table/Table'
 
 const Home = () => {
     return (
@@ -21,6 +22,12 @@ const Home = () => {
                 <div className="charts">
                     <Featured />
                     <Chart />
+                </div>
+                <div className="listContainer">
+                    <div className="listTitle">
+                        Latest Transactions
+                    </div>
+                    <List />
                 </div>
             </div>
         </div>
